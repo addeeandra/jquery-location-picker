b8ba3095f8add2dcba1220306225ed47936ec20a
Aditya Chandra <aditchndr44@gmail.com>
2017-01-23 21:08:07 +0700

    initial commit #2

---
+++ jquery-location-picker.js
@@ -0,0 +1,41 @@
+$(function ( $ ) {
+	var settings;
+
+	$.fn.locationPicker = function (mapOptions, options) {
+		settings = $.extend({
+			mapOptions: $.extend({
+				center: { lat: -7.259258, lng: 112.7931297 },
+				scrollwheel: true,
+				zoom: 17
+			}, mapOptions),
+			height: 320,
+			latInput: $(document.createElement('input')).attr('type', 'hidden').attr('name', 'latitude').addClass('location-picker-input-latitude'),
+			lngInput: $(document.createElement('input')).attr('type', 'hidden').attr('name', 'longitude').addClass('location-picker-input-longitude'),
+			mapElement: $(document.createElement('div'))
+		}, options);
+
+		settings.mapElement.css({ height: settings.height });
+		this.css({ height: settings.height })
+			.append(settings.mapElement)
+			.append(settings.latInput)
+			.append(settings.lngInput);
+
+		settings.map = new google.maps.Map(settings.mapElement[0], settings.mapOptions);
+		settings.marker = new google.maps.Marker({
+			position: settings.mapOptions.center,
+			map: settings.map,
+			title: 'Picked Location'
+		});
+
+		settings.map.addListener('click', function (e) {
+			settings.marker.setPosition(e.latLng);
+			settings.latInput.val(e.latLng.lat());
+			settings.lngInput.val(e.latLng.lng());
+		});
+
+		settings.latInput.val(settings.mapOptions.center.lat);
+		settings.lngInput.val(settings.mapOptions.center.lng);
+
+		return this;
+	};
+});
