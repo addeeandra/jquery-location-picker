b4dddfd377293151c9067f4bf7e21f9f6c9335c1
Aditya Chandra <aditchndr44@gmail.com>
2017-01-23 21:47:38 +0700

    add click callback

--- jquery-location-picker.js
+++ jquery-location-picker.js
@@ -11,7 +11,8 @@ $(function ( $ ) {
 			height: 320,
 			latInput: $(document.createElement('input')).attr('type', 'hidden').attr('name', 'latitude').addClass('location-picker-input-latitude'),
 			lngInput: $(document.createElement('input')).attr('type', 'hidden').attr('name', 'longitude').addClass('location-picker-input-longitude'),
-			mapElement: $(document.createElement('div'))
+			mapElement: $(document.createElement('div')),
+			click: function () {}
 		}, options);
 
 		settings.mapElement.css({ height: settings.height });
@@ -28,6 +29,8 @@ $(function ( $ ) {
 		});
 
 		settings.map.addListener('click', function (e) {
+			settings.click({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+			
 			settings.marker.setPosition(e.latLng);
 			settings.latInput.val(e.latLng.lat());
 			settings.lngInput.val(e.latLng.lng());
